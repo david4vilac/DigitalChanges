f1a753b3dc9343da6f81c7677754b2a23618bf53
david_avilac <adavila59@ucatolica.edu.co>
2022-06-09 13:31:47 -0500

    Despliegue en Heroku

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/xmen"
+SQLALCHEMY_DATABASE_URL = "postgresql://eolrzaltudmipr:d7bcd13fa27021249c2810f51bcc39a5d0bbf4261f1e7a4ce6e45538131b7532@ec2-52-44-13-158.compute-1.amazonaws.com:5432/d71vti205l5bai"
  
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
